ce02cd9f937e4652d07153b48ef809559f21d21a
Tyler H. <tyler.hubeny@uconn.edu>
2026-03-09 23:06:43 -0400

    implemented the link_records and deanonymization_rate functions

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -18,7 +18,22 @@ def link_records(anon_df, aux_df):
       anon_id, matched_name
     containing ONLY uniquely matched records.
     """
-    raise NotImplementedError
+    data = {'anon_id': [], 'matched_name': []}
+    for i in range(0, len(aux_df)):
+        aux_name = aux_df.iloc[i, 0]
+        pos_match = []
+        for j in range(0, len(anon_df)):
+            if aux_df.iloc[i, 1] == anon_df.iloc[j, 1]:
+                pos_match.append(j)
+        for k in range(2,4):
+            for x in range(0, len(anon_df)):
+                if (aux_df.iloc[i, k] != anon_df.iloc[x, k]) and (x in pos_match):
+                    pos_match.remove(x)
+        if len(pos_match) == 1:
+            data['anon_id'].append(pos_match[0])
+            data['matched_name'].append(aux_name)
+    df = pd.DataFrame(data)
+    return df
 
 
 def deanonymization_rate(matches_df, anon_df):
@@ -26,4 +41,4 @@ def deanonymization_rate(matches_df, anon_df):
     Compute the fraction of anonymized records
     that were uniquely re-identified.
     """
-    raise NotImplementedError
+    return len(matches_df) / len(anon_df)
